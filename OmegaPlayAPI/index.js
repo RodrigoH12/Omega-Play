f4dce1a0d13709cfb133985df85a199969d0a3c1
Carly42 <carla.andrea.cbm@gmail.com>
2021-04-21 16:40:27 -0400

    Adding API base project.

---
+++ OmegaPlayAPI/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan =  require('morgan');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+app.set('Port', process.env.PORT || 4004);
+
+app.use(morgan('dev'));
+
+app.listen(app.get('Port'), ()=>{
+    console.log(`Express server on port ${app.get('Port')}`);
+});
