77b39320d9e6c35cb4e57559efca708d0220b45a
Carly42 <carla.andrea.cbm@gmail.com>
2021-04-25 16:29:06 -0400

    Adding Movie and User database models.

---
+++ OmegaPlayAPI/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+const movies = mongoose.model("movies");
+const userSchema = new Schema({
+    userName: {type: String, require:true},
+    password: {type: String, require: true},
+    img: {type: String, require: true},
+    email: {type: String, require: true},
+    genre: {type: String, require: true},
+    birthDate: {type: Date, require: true},
+    country: {type: String, require: true},
+    city: {type: String, require: true},
+    movies: [{type : Schema.ObjectId, ref:"movies", require:false}] 
+});
+
+module.exports = mongoose.model('user', userSchema);
