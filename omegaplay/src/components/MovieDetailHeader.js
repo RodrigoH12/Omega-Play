9ae5cf5461b62240f6cb07bc39904b3b1889348d
Juan Garcia <juandigv69@gmail.com>
2021-05-05 22:13:10 -0400

    Implemented star rendering method for a movie calification

--- omegaplay/src/components/MovieDetailHeader.js
+++ omegaplay/src/components/MovieDetailHeader.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { Image, Row, Col, Jumbotron, Button} from "react-bootstrap";
+import React, { useState } from 'react'
+import { Image, Row, Col, Jumbotron, Button } from "react-bootstrap";
 import * as Icon from 'react-bootstrap-icons';
 
-export default function MovieDetailHeader({data}) {
+export default function MovieDetailHeader({ data }) {
+    const [showScore, setShowScore] = useState(false);
+
     const playButtonClick = (e) => {
         console.log("Pressed " + e);
     }
@@ -13,6 +15,44 @@ export default function MovieDetailHeader({data}) {
         });
         return result.slice(0, -2);
     };
+
+    const GetStars = (calification) => {
+        var key = 1;
+        var temp = calification.calification;
+        var result = '';
+        for (var i = 0; i < 5; i++) {
+            if (temp >= 0.5) {
+                if (temp > 1) {
+                    result = result + '2';
+                    temp -= 1;
+                } else {
+                    result = result + '1';
+                    temp -= 0.5;
+                }
+            } else {
+                result = result + '0';
+            }
+        }
+        const starList = Array.from(result);
+        const listItems = starList.map((c) => {
+            key++;
+            if (c > 0) {
+                if (c > 1) {
+                    c -= 1;
+                    return <Icon.StarFill key={key} />
+                } else {
+                    c -= 0.5;
+                    return <Icon.StarHalf key={key} />;
+                }
+            } else {
+                return <Icon.Star key={key} />
+            }
+        });
+        return (
+            <div>{listItems}</div>
+        );
+    };
+
     return (
         <Jumbotron fluid style={{ padding: '20px' }}>
             <Row>
@@ -21,15 +61,13 @@ export default function MovieDetailHeader({data}) {
                 </Col>
                 <Col align="left" md={4}>
                     <h1 >{data.title}</h1>
-                    <div>
-                        <Icon.StarFill />
-                        <Icon.StarFill />
-                        <Icon.StarFill />
-                        <Icon.StarFill />
-                        <Icon.Star />
+                    <div
+                        onMouseEnter={() => setShowScore(true)}
+                        onMouseLeave={() => setShowScore(false)}>
+                        {showScore ?
+                            <b><Icon.StarFill/> {data.calification}</b> : <GetStars calification={data.calification} /> }
                     </div>
                     <p>{data.duration}</p>
-
                     <br />
                     <br />
                     <p size="10px">Release Date: {data.releaseDate}</p>
