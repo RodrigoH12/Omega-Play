88fbae9bf09b7bd1fda8fc42c85ed31766f49ca0
Carly42 <carla.andrea.cbm@gmail.com>
2021-05-07 09:49:00 -0400

    Login controler correction

--- OmegaPlayAPI/controllers/user.controller.js
+++ OmegaPlayAPI/controllers/user.controller.js
@@ -1,46 +1,53 @@
 const UserCtrl = {}
 const User = require('../models/User')
 
-UserCtrl.login = async (req, res) =>{
-    const userName = req.query.userName;
-    const password = req.query.password;
+UserCtrl.login = async (req, res) => {
+    const userName = req.headers.username;
+    const password = req.headers.password;
     var currentUser = [];
 
     if (userName == undefined || password == undefined) {
         var currentUser = await User.find();
+        res.json(currentUser);
     } else {
         var currentUser = await User.find({ userName: userName, password: password });
+        if (currentUser.length <= 0) {
+            res.json({ "response": "A user with that credentials was not found" });
+        } else {
+            res.json(currentUser);
+        }
     }
-    
-    if(currentUser.length <= 0) {
-        res.json({"response": "A user with that credentials was not found"});
-    }
-
-    res.json(currentUser);
 }
 
-UserCtrl.registerUser = async (req, res) =>{
-    console.log ( req.body )
-    const userName = req.body.userName;
-    const password = req.body.password;
-    const email = req.body.email;
-    const genre = req.body.genre;
-    const birthDate = req.body.birthDate;
-    const country = req.body.country;
-    const city = req.body.country;
-    const UserObj = new User ({ userName, password, email, genre, birthDate, country, city });
-    await UserObj.save();
-    res.json({"response": "ok"});
+UserCtrl.registerUser = async (req, res) => {
+    var currentUser = await User.find({ userName: userName });
+    console.log(currentUser);
+    if (currentUser.length > 0) {
+        res.json({ "response": "A user with that username already exists" });
+    } else {
+        //console.log(req.body)
+        const userName = req.body.userName;
+        const password = req.body.password;
+        const email = req.body.email;
+        const genre = req.body.genre;
+        const birthDate = req.body.birthDate;
+        const country = req.body.country;
+        const city = req.body.city;
+        const UserObj = new User({ userName, password, email, genre, birthDate, country, city });
+        await UserObj.save();
+        console.log(UserObj);
+        res.json({ "response": "ok" });
+    }
 }
 
 UserCtrl.updateUserAvatar = async (req, res) => {
     const userName = req.query.userName;
     const avatar = req.query.avatar;
-    await User.updateOne({"userName": userName }, {"img": avatar});
+    await User.updateOne({ "userName": userName }, { "img": avatar });
     const updatedUser = await User.find({ userName: userName });
 
-    if(updatedUser.length <= 0) {
-        res.json({"response": "A user with that username was not found"});
+    if (updatedUser.length <= 0) {
+        res.json({ "response": "A user with that username was not found" });
     }
 
     res.json(updatedUser);
@@ -50,22 +57,22 @@ UserCtrl.deleteUser = async (req, res) => {
     const userName = req.query.userName;
 
     if (!userName) {
-        res.json({"response": "You must pass a username"});
+        res.json({ "response": "You must pass a username" });
     } else {
-        await User.deleteOne({"userName": userName});
+        await User.deleteOne({ "userName": userName });
     }
 
-    res.json({"response": "ok"});
+    res.json({ "response": "ok" });
 }
 
 UserCtrl.addMovieToHistory = async (req, res) => {
     const userName = req.query.userName;
     const movie = req.query.movie;
-    await User.updateOne({"userName": userName }, { $addToSet: { "movies": movie }});
+    await User.updateOne({ "userName": userName }, { $addToSet: { "movies": movie } });
     const updatedUser = await User.find({ userName: userName });
 
-    if(updatedUser.length <= 0) {
-        res.json({"response": "A user with that username was not found"});
+    if (updatedUser.length <= 0) {
+        res.json({ "response": "A user with that username was not found" });
     }
 
     res.json(updatedUser);
