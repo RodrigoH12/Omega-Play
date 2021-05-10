7463e10a4e2ef91ad51f289293fe91d2ceb84a64
Juan Garcia <juandigv69@gmail.com>
2021-05-09 21:51:32 -0400

    Removed unnecesary imports

--- omegaplay/src/components/ApplicationHeader/SideBarComponent.js
+++ omegaplay/src/components/ApplicationHeader/SideBarComponent.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { ListGroup, Row, Col } from "react-bootstrap";
 import * as Icon from "react-bootstrap-icons";
 import "../../stylesheets/SideBarComponent.css"
-import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
 export default function ApplicationHeaderComponent() {
@@ -11,13 +10,11 @@ export default function ApplicationHeaderComponent() {
         <>
             <ListGroup className="list-grouplink" align="left">
                 <ListGroup.Item action style={{ padding: "16px" }} onClick={()=>{history.push("/home")}}>
-                    
                         <Row>
                             <Col xs={1}></Col>
                             <Col xs={2}><Icon.House /></Col>
                             <Col>Home</Col>
                         </Row>
-                    
                 </ListGroup.Item>
                 <ListGroup.Item action style={{ padding: "16px" }} onClick={()=>{history.push("/movie")}} >
                     <Row >
--- omegaplay/src/components/MovieList/MovieCard.js
+++ omegaplay/src/components/MovieList/MovieCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import "../../stylesheets/MovieCard.css";
-import { Link } from "react-router-dom";
 
 export default function MovieCard({ data }) {
 
