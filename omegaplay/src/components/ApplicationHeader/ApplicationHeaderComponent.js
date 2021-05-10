454c0a1165b69a900c330870253f86e2cd846bc1
Juan Garcia <juandigv69@gmail.com>
2021-05-10 18:19:46 -0400

    Quick fix with toolbar redirect

--- omegaplay/src/components/ApplicationHeader/ApplicationHeaderComponent.js
+++ omegaplay/src/components/ApplicationHeader/ApplicationHeaderComponent.js
@@ -3,13 +3,14 @@ import { Button, Navbar, Nav, Image } from "react-bootstrap";
 import * as Icon from "react-bootstrap-icons";
 import SideBarComponent from "./SideBarComponent";
 import logo from "../../assets/logo.svg";
-import { Link } from "react-router-dom";
 import useStore from '../../zustand/login';
 import "../../stylesheets/ApplicationHeaderComponent.css";
+import { useHistory } from "react-router-dom";
 
 export default function ApplicationHeaderComponent(props) {
     const user = useStore(state => state.user);
     const [sidebar, setSidebar] = useState(true);
+    const history = useHistory();
 
     return (
         <div className="header-toolbar">
@@ -26,8 +27,8 @@ export default function ApplicationHeaderComponent(props) {
                             :
                             <div />
                         }
-                        <Navbar.Brand>
-                            <Image src={logo} width="50px"></Image>
+                        <Navbar.Brand onClick={()=>{history.push("/")}} className="navigationBarBrand">
+                            <Image src={logo} width="50px"></Image>{' '}
                             Omega Play
                         </Navbar.Brand>
                     </Nav>
