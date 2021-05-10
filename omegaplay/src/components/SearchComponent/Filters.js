2b8313756aeac02dbc3fdc3877e4cce32fc2363e
Rodrigo Heredia <rodrigoh1205@gmail.com>
2021-05-10 15:16:37 -0400

    Fix Filter dropdown buttons position and movieCarousel width

--- omegaplay/src/components/SearchComponent/Filters.js
+++ omegaplay/src/components/SearchComponent/Filters.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import { Dropdown, DropdownButton, Row } from "react-bootstrap";
 import useStore from '../../zustand/filters';
+import "../../stylesheets/MovieCard.css";
 
 export default function Filters() {
 
 	const {setGenre, setDate, setCountry} = useStore();
 
 	return (
-			<Row>
-				<DropdownButton id="dropdown-basic-button" variant="secondary" title="Genre" style={{margin: '10px'}} onSelect={(e) => setGenre(e)}>
+			<Row style={{justifyContent: 'center'}}>
+				<DropdownButton id="dropdown-basic-button" variant="secondary" title="Genre" onSelect={(e) => setGenre(e)}>
 					<Dropdown.Item eventKey="All">All</Dropdown.Item>
 					<Dropdown.Item eventKey="Action">Action</Dropdown.Item>
 					<Dropdown.Item eventKey="Superheroes">Superheroes</Dropdown.Item>
                     <Dropdown.Item eventKey="Thriller">Thriller</Dropdown.Item>
 				</DropdownButton>
-				<DropdownButton id="dropdown-basic-button" variant="secondary" title="Date" style={{margin: '10px'}} onSelect={(e) => setDate(e)}>
+				<DropdownButton id="dropdown-basic-button" variant="secondary" title="Date" onSelect={(e) => setDate(e)}>
 					<Dropdown.Item eventKey="All" >All</Dropdown.Item>
                     <Dropdown.Item eventKey="This Week">This week</Dropdown.Item>
 					<Dropdown.Item eventKey="This Month">This month</Dropdown.Item>
                     <Dropdown.Item eventKey="This Year">This year</Dropdown.Item>
 				</DropdownButton>
-				<DropdownButton id="dropdown-basic-button" variant="secondary" title="Country" style={{margin: '10px'}} onSelect={(e) => setCountry(e)}>
+				<DropdownButton id="dropdown-basic-button" variant="secondary" title="Country"onSelect={(e) => setCountry(e)}>
 					<Dropdown.Item eventKey="All">All</Dropdown.Item>
 					<Dropdown.Item eventKey="United States">United States</Dropdown.Item>
 					<Dropdown.Item eventKey="Bolivia">Bolivia</Dropdown.Item>
