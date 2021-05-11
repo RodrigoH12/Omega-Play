79baf106c3c1bcb45165386b587286a1b6d0ec08
Rodrigo Heredia <rodrigoh1205@gmail.com>
2021-05-11 14:46:47 -0400

    Implement methods to fill filters dynamically

--- omegaplay/src/components/SearchComponent/Filters.js
+++ omegaplay/src/components/SearchComponent/Filters.js
@@ -1,30 +1,74 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Dropdown, DropdownButton, Row } from "react-bootstrap";
-import useStore from '../../zustand/filters';
 import "../../stylesheets/MovieCard.css";
+import useStore from '../../zustand/filters';
+import useStoreMovies from '../../zustand/movies';
 
 export default function Filters() {
+	const [genreTitle, setGenreTitle] = useState("All");
+	const [dateTitle, setDateTitle] = useState("All");
+	const [countryTitle, setCountryTitle] = useState("All");
 
 	const {setGenre, setDate, setCountry} = useStore();
 
+	const { movies } = useStoreMovies();
+
+	const getAllGenres = () => {
+		var genresArray = [];
+		
+		movies.forEach(movie => {
+			movie.genres.forEach(genre => {
+				checkDuplicateValue(genresArray, genre);
+			});
+		});
+
+		return genresArray;
+	};
+
+	const getAllCountries = () => {
+		var countriesArray = [];
+
+		movies.forEach(movie => {
+			checkDuplicateValue(countriesArray, movie.country);
+		});
+
+		return countriesArray;
+	};
+
+	const checkDuplicateValue = (array, value) => {
+		if (array.includes(value) === false) array.push(value);
+	}
+
 	return (
 			<Row style={{justifyContent: 'center'}}>
-				<DropdownButton id="dropdown-basic-button" variant="secondary" title="Genre" onSelect={(e) => setGenre(e)}>
+				<p className="FilterTitles">Genre:</p>
+				<DropdownButton id="dropdown-basic-button" variant="secondary" title={genreTitle} onSelect={(e) => {setGenre(e); setGenreTitle(e)}}>
 					<Dropdown.Item eventKey="All">All</Dropdown.Item>
-					<Dropdown.Item eventKey="Action">Action</Dropdown.Item>
-					<Dropdown.Item eventKey="Superheroes">Superheroes</Dropdown.Item>
-                    <Dropdown.Item eventKey="Thriller">Thriller</Dropdown.Item>
+					{
+					getAllGenres().map((genre) => {
+						return (
+							<Dropdown.Item key={genre} eventKey={genre}>{genre}</Dropdown.Item>
+						);
+					})
+					}
 				</DropdownButton>
-				<DropdownButton id="dropdown-basic-button" variant="secondary" title="Date" onSelect={(e) => setDate(e)}>
+				<p className="FilterTitles">Date:</p>
+				<DropdownButton id="dropdown-basic-button" variant="secondary" title={dateTitle} onSelect={(e) => {setDate(e); setDateTitle(e)}}>
 					<Dropdown.Item eventKey="All" >All</Dropdown.Item>
                     <Dropdown.Item eventKey="This Week">This week</Dropdown.Item>
 					<Dropdown.Item eventKey="This Month">This month</Dropdown.Item>
                     <Dropdown.Item eventKey="This Year">This year</Dropdown.Item>
 				</DropdownButton>
-				<DropdownButton id="dropdown-basic-button" variant="secondary" title="Country"onSelect={(e) => setCountry(e)}>
+				<p className="FilterTitles">Country:</p>
+				<DropdownButton id="dropdown-basic-button" variant="secondary" title={countryTitle} onSelect={(e) => {setCountry(e); setCountryTitle(e)}}>
 					<Dropdown.Item eventKey="All">All</Dropdown.Item>
-					<Dropdown.Item eventKey="United States">United States</Dropdown.Item>
-					<Dropdown.Item eventKey="Bolivia">Bolivia</Dropdown.Item>
+					{
+					getAllCountries().map((country) => {
+						return (
+							<Dropdown.Item key={country} eventKey={country}>{country}</Dropdown.Item>
+						);
+					})
+					}
 				</DropdownButton>
 			</Row>
 	);
